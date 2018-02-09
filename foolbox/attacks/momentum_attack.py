09f75fb80cb36edf516b48d19b74184e4652c4ea
neale <nealeratzlaff@gmail.com>
2018-02-08 18:48:27 -0800

    added multiple rounds of testing

--- foolbox/attacks/momentum_attack.py
+++ foolbox/attacks/momentum_attack.py
@@ -11,7 +11,7 @@ class MomentumIterativeAttack(Attack):
         eta = np.clip(eta, -eps, eps)
         return eta
 
-    def _apply(self, a, epsilon=0.5, eps_iter=0.06, nb_iter=10):
+    def _apply(self, a, epsilons=[0.3, 0.5, 1.0], eps_iter=0.06, nb_iter=10):
         if not a.has_gradient():
             return
 
@@ -19,23 +19,29 @@ class MomentumIterativeAttack(Attack):
         min_, max_ = a.bounds()
         momentum = 0
         perturbed = image
-        for i in range(nb_iter):
-            #compute grad wrt image and label
-            grad = a.gradient() 
-            # normalize gradiet and add
-            red_ind = list(range(1, len(grad.shape)))
-            # avoid zero grad
-            mean_grad = np.mean(np.abs(grad))
-            grad = grad / np.maximum(1e-12, mean_grad)
-            #add decay factor, default 1.0
-            momentum = 1.0 * momentum + grad
-            normalized_grad = np.sign(momentum)
-            scaled_grad = eps_iter * normalized_grad
-            perturbed = perturbed + scaled_grad
-            perturbed = image + self.clip_ball(perturbed-image, epsilon)
-            if min_ is not None and max_ is not None:
-                perturbed = np.clip(perturbed, min_, max_)
-        
+        for epsilon in epsilons:
+            for i in range(nb_iter):
+                #compute grad wrt image and label
+                grad = a.gradient() 
+                # normalize gradiet and add
+                red_ind = list(range(1, len(grad.shape)))
+                # avoid zero grad
+                mean_grad = np.mean(np.abs(grad))
+                grad = grad / np.maximum(1e-12, mean_grad)
+                #add decay factor, default 1.0
+                momentum = 1.0 * momentum + grad
+                normalized_grad = np.sign(momentum)
+                scaled_grad = eps_iter * normalized_grad
+                perturbed = perturbed + scaled_grad
+                perturbed = image + self.clip_ball(perturbed-image, epsilon)
+                if min_ is not None and max_ is not None:
+                    perturbed = np.clip(perturbed, min_, max_)
+                
+                logits, is_adversarial = a.predictions(perturbed)
+                #print "iter {} prediction: {}".format(i, np.argmax(logits))
+                if is_adversarial:
+                    return
+
         return
 
 
--- test.py
+++ test.py
@@ -14,22 +14,22 @@ from scipy.misc import imsave, imshow, imread
 import matplotlib.pyplot as plt
 import glob
 
-image_dir = './imgs'
+image_dir = './imagenet224'
 
 keras.backend.set_learning_phase(0)
 kmodel = ResNet50(weights='imagenet')
 preprocessing = (np.array([104, 116, 123]), 1)
 fmodel = KerasModel(kmodel, bounds=(0, 255), preprocessing=preprocessing)
 
-
+success = 0.
 paths = glob.glob(image_dir+'/*.png')
-for path in paths:
+for path in paths[:100]:
     image = imread(path).astype(np.float32)
 
-    test = image.copy().astype(np.float32)
+    test = image.copy()
     preds = kmodel.predict(preprocess_input(np.expand_dims(test, 0)))
     label = np.argmax(preds)
-    print("Top 3 predictions (regular: ", decode_predictions(preds, top=3))
+    #print("Top 3 predictions (regular: ", decode_predictions(preds, top=3))
     
     # run the attack
     print "running the attack"
@@ -40,23 +40,37 @@ for path in paths:
         print "Did not find an adversarial"
         continue
     # show results
-    print(np.argmax(fmodel.predictions(adversarial)))
     print(foolbox.utils.softmax(fmodel.predictions(adversarial))[781])
     adversarial_rgb = adversarial[np.newaxis, :, :, ::-1]
     preds = kmodel.predict(preprocess_input(adversarial_rgb.copy()))
-    print("Top 5 predictions (adversarial: ", decode_predictions(preds, top=5))
-
+    adv_label = np.argmax(preds)
+    if adv_label != label:
+        success += 1
+    #print("Top 5 predictions (adversarial: ", decode_predictions(preds, top=5))
+    
     diff = (adversarial_rgb[0] - image)
+
+    # normalize to 0-1 for viewing with matplotlib
     a = adversarial_rgb[0].copy()
     nx = (a-np.min(a))/(np.max(a)-np.min(a))
     nd = (diff-np.min(diff))/(np.max(diff)-np.min(diff))
-    plt.figure()
-    plt.subplot(1, 2, 1)
+    
+    max_norm = np.max(np.abs(diff))
+    print "max norm: ", max_norm
+    print "l2 norm: ", np.linalg.norm(diff)
+    print "---------------------------------"
+    
+    plt.ion()
+    plt.subplot(1, 3, 1)
+    plt.title("Real")
+    plt.imshow(image.astype(np.uint8))
+    plt.subplot(1, 3, 2)
+    plt.title("eps: {}".format(max_norm))
     plt.imshow(nx)
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 3)
+    plt.title("Mask")
     plt.imshow(nd)
     plt.savefig('adv_test', format='png')
-    plt.show()
-    print "l2 norm: ", np.linalg.norm(diff)
-    print "max norm: ", np.max(np.abs(adversarial_rgb-image))
+    plt.draw()
 
+print "Success percentage: {}%".format(success)
